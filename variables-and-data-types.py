fb72dd523fada7a8acf1fb99e35062335d45197e
Gitthuma <george.githuma@gmail.com>
2021-08-17 15:14:09 +0300

    Created variables and swapped them in the place of name and age

--- variables-and-data-types.py
+++ variables-and-data-types.py
@@ -1,4 +1,9 @@
-print("There once was a man named George,")
-print("he was 70 years old.")
-print("He really liked the name George,")
-print("but didn't like being 70.")
+# Create variable
+character_name = "John"
+character_age = "35"
+
+# Swap name and age for variables
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print("He really liked the name " + character_name + ",")
+print("but didn't like being" + character_age + ".")
