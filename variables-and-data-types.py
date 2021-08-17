cdcee426d6c0f4cb15511e87d8859df5017bfe07
Gitthuma <george.githuma@gmail.com>
2021-08-17 17:15:20 +0300

    Changed character_age from string to int in variable assignment and converted character_age inside the print statement to string

--- variables-and-data-types.py
+++ variables-and-data-types.py
@@ -1,13 +1,14 @@
 # Create variable
+# Changed character_age from string to int in variable assignment and converted character_age inside the print statement to string
 character_name = "John"
-character_age = "35"
+character_age = 50
 
 # Swap name and age for variables
 print("There once was a man named " + character_name + ",")
-print("he was " + character_age + " years old.")
+print("he was " + str(character_age) + " years old.")
 
 # Update variables mid story
 character_name = "George"
-character_age = "45"
+character_age = 50 + 10
 print("He really liked the name " + character_name + ",")
-print("but didn't like being " + character_age + ".")
+print("but didn't like being " + str(character_age) + ".")
